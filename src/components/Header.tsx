a662f92fbb950ea5df7d44ce7b4087c8336b56f0
Misa Takeda <misatakeda.2799@gmail.com>
2024-10-16 21:30:17 +0900

    create Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-[#da627d] p-6">
+      <h1 id="headerTittle" className="text-xl text-[#f9f4f5] text-center">
+        Sachiko and Komugi's weather
+      </h1>
+    </header>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      tablet: "820px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+    },
   },
   plugins: [],
 };
